509c1eeae7a058ae8218da234aacce42fb4f196e
Andrea <andrea.novello.dev@gmail.com>
2022-03-21 16:38:58 +0100

    fixed DAOConstants

--- project/src/main/java/project/architecture/dao/DAOConstants.java
+++ project/src/main/java/project/architecture/dao/DAOConstants.java
@@ -3,5 +3,4 @@ package project.architecture.dao;
 public interface DAOConstants {
 	public static String SELECT_DOCENTI = "Select * from docente";
 	public static String SELECT_DOCENTE_BY_PK = "Select * from docente where coddocente = ?";
-	public static String SELECT_DOCENTE_MORE_CORSO = "Select unique nomedocente, cognomedocente from docente, corso where docente.coddocente = corso.coddocente";
 }
