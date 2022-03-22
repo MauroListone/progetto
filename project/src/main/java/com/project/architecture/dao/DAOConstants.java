b031f9b507c5669bac86fcfe13f7e3c6cc9d79ba
Betacom <Betacom@Betacom-PC903F.fritz.box>
2022-03-22 15:34:13 +0100

    Correzione query DELETE_CORSISTA

--- project/src/main/java/com/project/architecture/dao/DAOConstants.java
+++ project/src/main/java/com/project/architecture/dao/DAOConstants.java
@@ -8,7 +8,7 @@ public interface DAOConstants {
 	String SELECT_CORSISTA = "Select * from corsista";
 	
 	String SELECT_CORSISTA_BY_PK = "Select * from corsista where codcorsista = ?";
-	String DELETE_CORSISTA = "Delete from corsista where codcorso = ?";
+	String DELETE_CORSISTA = "Delete from corsista where codcorsista = ?";
 	
 	String UPDATE_CORSISTA = "Update corsista set nomecorsista = ? , cognomecorsista = ? where codcorso = ?";
 	
